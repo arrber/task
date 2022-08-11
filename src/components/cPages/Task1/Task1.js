dda84d4b3aea07ca7ad19c848ca755302a645374
dion-berisha <72410561+dion-berisha@users.noreply.github.com>
2022-08-12 01:49:51 +0200

    trending giphy

--- src/components/cPages/Task1/Task1.js
+++ src/components/cPages/Task1/Task1.js
@@ -1,12 +1,40 @@
 import React from "react";
-import "./Task1.scss";
+import {useState, useEffect} from "react";
+import axios from 'axios';
+import styles from '../Task1/Task1.scss';
 
-const Task1 = () => {
+
+const Giphy = () => {
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        const results = async () => {
+            const res = await axios('https://api.giphy.com/v1/gifs/trending', {
+                params: {
+                    api_key: "f254wg9cnrbVs9uGJgvRVasDmzFvMsGG"
+                }
+        })
+            console.log(res);
+            setData(res.data.data)
+        }
+
+        results(); 
+    }, [])
+
+    const gifRender = () => {
+        return data.map(gif => {
+            return(
+                <div key={gif.id} className={styles.bodyContainer}>
+                    <img src={gif.images.fixed_height.url}/>
+                </div>
+            )
+            
+        })
+    }
     return(
         <div className="Giphy">
-            
+            {gifRender()}
         </div>
     );
 };
 
-export default Task1;
+export default Giphy;
