a100c70bac55195442b075f32a8f07f33812faf6
arber <arberbreznica98@gmail.com>
2022-08-12 01:56:38 +0200

    completing task3

--- src/components/cPages/Task3/Task3.js
+++ src/components/cPages/Task3/Task3.js
@@ -1,9 +1,36 @@
 import React from "react";
 
+import "./Task3.scss";
+
 const Task3 = () => {
+    const programmerOutput = [
+        {
+            name: "Uncle Bobby",
+            linesOfCode: 500
+        },
+        {
+            name: "Suzie Q",
+            linesOfCode: 1500
+        },
+        {
+            name: "Jimmy Gosling",
+            linesOfCode: 150
+        },
+        {
+            name: "Gracie Hopper",
+            linesOfCode: 1000
+        }
+    ];
+       
+    let totalOutput = 0;
+       
+    for (let i = 0; i < programmerOutput.length; i++) {
+        totalOutput += programmerOutput[i].linesOfCode;
+       }
+
     return(
         <div className="Code">
-            
+            <h1>{totalOutput}</h1>
         </div>
     );
 };
