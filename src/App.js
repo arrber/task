eff053a4ee19221e79418945abf4fff9c221a0ca
arber <arberbreznica98@gmail.com>
2022-08-11 23:09:43 +0200

    error on importing app scss

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import "App.scss";
+import "./App.scss";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>test</h1>
+        <h1>testn</h1>
       </div>
     );
   }
