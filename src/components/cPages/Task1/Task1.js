7e828de349e9a8b96b618225f8254d779ea5afe3
dion-berisha <72410561+dion-berisha@users.noreply.github.com>
2022-08-12 09:10:48 +0200

    Search bar fixed when the string is null

--- src/components/cPages/Task1/Task1.js
+++ src/components/cPages/Task1/Task1.js
@@ -1,85 +1,78 @@
 import React from "react";
-import {useState, useEffect} from "react";
-import axios from 'axios';
-import styles from '../Task1/Task1.scss';
-
+import { useState, useEffect } from "react";
+import axios from "axios";
+import styles from "../Task1/Task1.scss";
 
 const Giphy = () => {
-    const [data, setData] = useState([]);
-    const [search, setSearch] = useState("");
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState("");
 
-    useEffect(() => {
-        const fetchData = async () => {
-          try {
-            const results = await axios("https://api.giphy.com/v1/gifs/trending", {
-              params: {
-                api_key: "tAEFUgagRjRNkU24orQdFB8EHMcNTUSe",
-                limit: 100
-              }
-            });
-    
-            console.log(results);
-            setData(results.data.data);
-          } catch{}
-        };
-    
-        fetchData();
-      }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const results = await axios("https://api.giphy.com/v1/gifs/trending", {
+          params: {
+            api_key: "tAEFUgagRjRNkU24orQdFB8EHMcNTUSe",
+            limit: 100,
+          },
+        });
+        setData(results.data.data);
+      } catch {}
+    };
+    fetchData();
+  }, []);
 
-    const gifRender = () => {
-        return data.map(gif => {
-            return(
-             <div key={gif.id} className={styles.bodyContainer}>
-                <img src={gif.images.fixed_height.url}/>
-             </div>
-            )
-        })
-    }
-
-    const handleSearchChange = event => {
-        setSearch(event.target.value);
-      };
-    
-      const handleSubmit = async event => {
-        event.preventDefault();
-    
-        try {
-          const results = await axios("https://api.giphy.com/v1/gifs/search", {
-            params: {
-              api_key: "tAEFUgagRjRNkU24orQdFB8EHMcNTUSe",
-              q: search,
-              limit: 20
-            }
-          });
-          setData(results.data.data);
-        } catch{}
-      };
-
-    return(
-        <div className="Giphy">
-            <form className="formContainer">
-                <input
-                    value={search}
-                    onChange={handleSearchChange}
-                    type="text"
-                    placeholder="Search..."
-                    className="searchInput"
-                />
-                <button
-                    onClick={handleSubmit}
-                    type="submit"
-                    className="searchButton"
-                    >
-                    Search
-                </button>
-            </form>
-            <br/>
-            {gifRender()}
+  const gifRender = () => {
+    return data.map((gif) => {
+      return (
+        <div key={gif.id} className={styles.bodyContainer}>
+          <img src={gif.images.fixed_height.url} />
         </div>
-    );
+      );
+    });
+  };
 
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value);
+  };
 
-    
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (search === "") {
+      gifRender();
+    } else {
+      try {
+        const results = await axios("https://api.giphy.com/v1/gifs/search", {
+          params: {
+            api_key: "tAEFUgagRjRNkU24orQdFB8EHMcNTUSe",
+            q: search,
+            limit: 20,
+          },
+        });
+        setData(results.data.data);
+      } catch {}
+    }
+  };
+
+  return (
+    <div className="Giphy">
+      <form className="formContainer">
+        <input
+          value={search}
+          onChange={handleSearchChange}
+          type="text"
+          placeholder="Search..."
+          className="searchInput"
+        />
+        <button onClick={handleSubmit} type="submit" className="searchButton">
+          Search
+        </button>
+      </form>
+      <br />
+      {gifRender()}
+    </div>
+  );
 };
 
 export default Giphy;
