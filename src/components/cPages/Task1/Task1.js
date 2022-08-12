3e68621b7376b494f316c84fa2f012a32bbe9238
dion-berisha <72410561+dion-berisha@users.noreply.github.com>
2022-08-12 03:14:06 +0200

    Last edit on the task

--- src/components/cPages/Task1/Task1.js
+++ src/components/cPages/Task1/Task1.js
@@ -6,35 +6,80 @@ import styles from '../Task1/Task1.scss';
 
 const Giphy = () => {
     const [data, setData] = useState([]);
-    useEffect(() => {
-        const results = async () => {
-            const res = await axios('https://api.giphy.com/v1/gifs/trending', {
-                params: {
-                    api_key: "f254wg9cnrbVs9uGJgvRVasDmzFvMsGG"
-                }
-        })
-            console.log(res);
-            setData(res.data.data)
-        }
+    const [search, setSearch] = useState("");
 
-        results(); 
-    }, [])
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const results = await axios("https://api.giphy.com/v1/gifs/trending", {
+              params: {
+                api_key: "tAEFUgagRjRNkU24orQdFB8EHMcNTUSe",
+                limit: 100
+              }
+            });
+    
+            console.log(results);
+            setData(results.data.data);
+          } catch{}
+        };
+    
+        fetchData();
+      }, []);
 
     const gifRender = () => {
         return data.map(gif => {
             return(
-                <div key={gif.id} className={styles.bodyContainer}>
-                    <img src={gif.images.fixed_height.url}/>
-                </div>
+             <div key={gif.id} className={styles.bodyContainer}>
+                <img src={gif.images.fixed_height.url}/>
+             </div>
             )
-            
         })
     }
+
+    const handleSearchChange = event => {
+        setSearch(event.target.value);
+      };
+    
+      const handleSubmit = async event => {
+        event.preventDefault();
+    
+        try {
+          const results = await axios("https://api.giphy.com/v1/gifs/search", {
+            params: {
+              api_key: "tAEFUgagRjRNkU24orQdFB8EHMcNTUSe",
+              q: search,
+              limit: 20
+            }
+          });
+          setData(results.data.data);
+        } catch{}
+      };
+
     return(
         <div className="Giphy">
+            <form className="formContainer">
+                <input
+                    value={search}
+                    onChange={handleSearchChange}
+                    type="text"
+                    placeholder="Search..."
+                    className="searchInput"
+                />
+                <button
+                    onClick={handleSubmit}
+                    type="submit"
+                    className="searchButton"
+                    >
+                    Search
+                </button>
+            </form>
+            <br/>
             {gifRender()}
         </div>
     );
+
+
+    
 };
 
 export default Giphy;
